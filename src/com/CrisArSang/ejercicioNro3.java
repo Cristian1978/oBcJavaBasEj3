a32bc5039b4ca2379c56b98002e7ef89406aa60f
Cristian1978 <cris.ar.sang@gmail.com>
2022-11-05 21:23:55 -0300

    1

---
+++ src/com/CrisArSang/ejercicioNro3.java
@@ -0,0 +1,16 @@
+package com.CrisArSang;
+
+public class ejercicioNro3 {
+
+    public static void main(String[] args) {
+
+        String[] amigos = new String[]{" Alejandro", " Julio", " Horacio",
+                " Angel"};
+
+
+        for (int i = 0; i < amigos.length; i++) {
+            System.out.print(amigos[i]);
+        }
+    }
+}
+
